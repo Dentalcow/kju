cb7569caf320c005c43a6d1b50c3b48a9db2e3ca
DentalCow <87558010+Dentalcow@users.noreply.github.com>
2024-12-04 21:00:06 +1100

    awesome sauce

--- kju/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/kju.AssemblyInfo.cs
+++ kju/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/kju.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kju")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5e8891060ae4209955c989c9e0e12a3e68023f")]
 [assembly: System.Reflection.AssemblyProductAttribute("kju")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kju")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
